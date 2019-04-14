e90eab67199f884244569a39b1088b9aafbf810c
Ivan Shambin <Jirimoarichi@gmail.com>
2019-04-14 22:08:17 +0300

    Добавлен режим

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,11 +15,19 @@ public class Main {
     public static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main (String[] args) throws SQLException, IOException {
-
         VocabularyService vocService = new VocabularyService();
         Vocabulary voc1 = new Vocabulary();
-        UserInterfaceService userInterface = new UserInterfaceService(vocService, voc1);
-        userInterface.showInterface();
+        int i = 10;
+        while (i!=0) {
+            System.out.println("Выберете режим \n1. Управление БД \n0. Выход");
+            i = Integer.parseInt(bufferedReader.readLine());
+            switch (i) {
+                case 1: UserInterfaceService userInterface = new UserInterfaceService(vocService, voc1);
+                        userInterface.showInterface();
+                        break;
+            }
+        }
+
     }
 
 
